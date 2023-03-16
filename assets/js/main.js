fadca519e1fc678dc93a0f45b2760ef8512bbf7a
João Oliveira <joaooliveira1999@outlook.com>
2023-03-16 01:20:40 -0300

    update pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,65 @@
 
 // responsive
 
-const menu = document.querySelector(".menu_responsive");
+const menu = document.querySelector(".menu");
 const NavMenu = document.querySelector(".nav-menu");
 
 menu.addEventListener("click", () => {
   menu.classList.toggle("ativo");
   NavMenu.classList.toggle("ativo");
 });
+
+//formulario
+const form = document.getElementById("form");
+const campos = document.querySelectorAll(".required");
+const spans = document.querySelectorAll(".span-required");
+const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  nameValidate();
+  emailValidate();
+});
+
+function setError(index) {
+  campos[index].style.border = "2px solid #FF0000";
+  spans[index].style.display = "block";
+}
+
+function removeError(index) {
+  campos[index].style.border = "";
+  spans[index].style.display = "none";
+}
+
+function nameValidate() {
+  if (campos[0].value.length < 3) {
+    setError(0);
+  } else {
+    removeError(0);
+  }
+}
+
+function emailValidate() {
+  if (!emailRegex.test(campos[1].value)) {
+    setError(1);
+  } else {
+    removeError(1);
+  }
+}
+
+function mainPasswordValidate() {
+  if (campos[2].value.length < 8) {
+    setError(2);
+  } else {
+    removeError(2);
+    comparePassword();
+  }
+}
+
+function comparePassword() {
+  if (campos[2].value == campos[3].value && campos[3].value.length >= 8) {
+    removeError(3);
+  } else {
+    setError(3);
+  }
+}
